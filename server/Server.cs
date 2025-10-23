ddaa901eff48f11435d3ba9a0e0d0c377ea65865
capyvankov <capyvankov@gmail.com>
2025-10-23 23:43:06 +0300

    Начата работа над сервером
    Добавлен класс Server

---
+++ server/Server.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dataCentre.server
+{
+    internal class Server
+    {
+        public required int serverNum;
+        public int temp = 0;
+        public int load = 0;
+        public int memoryUse = 0;
+        public int powerDraw = 0;
+
+    }
+}
